6ef46f09c81cd69f85c0b04218e0f921c2a5d982
tenaligopireddy <70321818+tenaligopireddy@users.noreply.github.com>
2020-08-27 19:17:32 +0530

    Add files via upload

---
+++ CONFIG_SERVER/trade-config-server/trade-config-server/src/test/java/com/tradingsystem/microservice/tradeconfigserver/TradeConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradingsystem.microservice.tradeconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
